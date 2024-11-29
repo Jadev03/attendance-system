db08f548afd66d9249d783659e7b449026daa20d
Jadev03 <sathiendrathabendra@gmail.com>
2024-11-29 21:25:31 +0530

    #2, server setup sucessfully

---
+++ server/Httpserver.java
@@ -0,0 +1,16 @@
+package server;
+
+import com.sun.net.httpserver.*;
+import java.io.*;
+import java.net.InetSocketAddress;
+
+public class Httpserver{
+    public static void main(String[] args) throws IOException {
+        int port=8080; // define backend server running port number 8080
+        HttpServer server= HttpServer.create(new InetSocketAddress(port), 0);
+        System.out.println("Server is running on http://localhost:8080 ..........");
+        server.start();      
+    }
+
+
+}
